2660d09cbb3a2a25b53a2cee1ab34f89f56266f1
Nishaant Dhingra <dhingranishaant@gmail.com>
2026-05-06 10:14:41 -0400

    restoring-github-pages-health

--- sre-portfolio/src/App.js
+++ sre-portfolio/src/App.js
@@ -6,7 +6,7 @@ import { Toaster } from "./components/ui/sonner";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Portfolio />} />
         </Routes>
